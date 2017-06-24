f1266ca1d3a8c4b1e5220dcbde2e150ad6eec2d1
Jisoo Hwang <jisoo1201@naver.com>
2017-06-24 12:44:09 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -41,14 +42,6 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
-  # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
     
    # Confirms the correct user.
     def correct_user
@@ -62,11 +55,21 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
   
-   private
+  private
+
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    # Before filters
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
     # Confirms an admin user.
     def admin_user
       redirect_to(root_url) unless current_user.admin?
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ root 'static_pages#home'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
